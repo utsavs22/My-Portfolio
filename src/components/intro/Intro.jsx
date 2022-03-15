5716ba4c26107153bfdbb69ae620166ce28b34a2
Utsav Sinha <utsavsinha22.official@gmail.com>
2022-03-15 22:42:25 +0530

    intro page done

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-        
+        <div className="left">
+          <div className="imageContainer">
+            <img src="assets/man.png" alt="" />
+          </div>
+        </div>
+        <div className="right">
+          <div className="wrapper">
+            <h2>Hi There, I'm</h2>
+            <h1>Utsav Sinha</h1>
+            <h3><span>Web Developer</span> and<span> Learner</span></h3>
+          </div>
+          <a href="#portfolio">
+            <img src="assets/down.png" alt="" />
+          </a>
+        </div>
     </div>
   )
 }
