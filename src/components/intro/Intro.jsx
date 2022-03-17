a33d701ba6980757e744d0af56efed215b53c0eb
Utsav Sinha <utsavsinha22.official@gmail.com>
2022-03-17 23:59:40 +0530

    h

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,7 +5,7 @@ export default function Intro() {
     <div className="intro" id="intro">
         <div className="left">
           <div className="imageContainer">
-            <img src="assets/man.png" alt="" />
+            <img src="assets/man.png" alt=""/>
           </div>
         </div>
         <div className="right">
