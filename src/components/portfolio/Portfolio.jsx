5e2abda45101f773885c0743e4fb0249223e7e8b
Utsav Sinha <utsavsinha22.official@gmail.com>
2022-04-28 17:18:06 +0530

    made tehe layout for portfolio  section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,40 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Features</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Designing</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="https://i.redd.it/4rz9wpyvpbb31.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://i.redd.it/4rz9wpyvpbb31.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://i.redd.it/4rz9wpyvpbb31.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://i.redd.it/4rz9wpyvpbb31.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://i.redd.it/4rz9wpyvpbb31.jpg" width="100px" alt="" />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img src="https://i.redd.it/4rz9wpyvpbb31.jpg" width="100px" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   )
 }
